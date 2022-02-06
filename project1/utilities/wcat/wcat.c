18cd7467d50edcc45def87b25cbf2336826968a3
Alex Rodriguez <alejandro.rodriguezlimon@utexas.edu>
2022-02-05 18:42:41 -0600

    passing all 7 tests for wcat

--- project1/utilities/wcat/wcat.c
+++ project1/utilities/wcat/wcat.c
@@ -1,1 +1,33 @@
-//Hey your code goes here..!
+//Your code goes here..!
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    FILE *in_stream = NULL;
+    size_t nread = 0;
+    size_t len = 0;
+    char *line = NULL;
+
+    for (uint32_t i = 1; i < argc; i++) {
+        in_stream = fopen(argv[i], "r");
+        if (in_stream == NULL) {
+            fprintf(stdout, "wcat: cannot open file\n");
+            exit(1);
+        }
+
+        while ((nread = getline(&line, &len, in_stream)) != -1) {
+            fprintf(stdout, "%s", line);
+        }
+
+        free(line);
+    }
+
+    return 0;
+}

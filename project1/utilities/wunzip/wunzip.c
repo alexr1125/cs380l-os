6a0fcdc2dc5d3c4063e9c3b7bde5b25fa4cd7719
Alex Rodriguez <alejandro.rodriguezlimon@utexas.edu>
2022-02-05 19:53:55 -0600

    passing all 6 tests for wunzip

--- project1/utilities/wunzip/wunzip.c
+++ project1/utilities/wunzip/wunzip.c
@@ -1,1 +1,38 @@
-//Hey your code goes here..!
+//Your code goes here..!
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    FILE *in_stream = NULL;
+    char line[5];
+
+    if (argc <= 1) {
+        fprintf(stdout, "wunzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    for (uint32_t i = 1; i < argc; i++) {
+        in_stream = fopen(argv[i], "r");
+        if (in_stream == NULL) {
+            fprintf(stdout, "wcat: cannot open file\n");
+            exit(1);
+        }
+
+        while (fread(line, 5, 1, in_stream)) {
+            uint32_t num_chars = *((uint32_t *) line);
+            char char_to_print = line[4];
+            for(uint32_t i = 0; i < num_chars; i++) {
+                fprintf(stdout, "%c", char_to_print);
+            }
+        }
+    }
+
+    return 0;
+}
